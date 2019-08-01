694ee86a10c2931a30af845c2d522e4a5ab963a9
Bruce Ruffin <bruffin1221@gmail.com>
2019-08-01 18:22:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,1 +1,13 @@
+def my_each(words)
+  letter=0
 
+  while letter<words.length
+    yields array[letter]
+    letter+=1
+  end
+  array
+end
+
+my_each(["arel", "jon", "logan", "spencer"]) do |letter|
+  puts letter
+end 
